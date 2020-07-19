0eb8000feea9195babb09cb3061cea4636acf991
Ahn YoungSeon <dudtjs0920@naver.com>
2020-07-20 01:18:51 +0900

    weather 부분 수정

--- src/App.tsx
+++ src/App.tsx
@@ -12,12 +12,15 @@ import {
   StyleSheet,
   View,
   ScrollView,
-  Text
+  Text,
 } from 'react-native';
 import styled from 'styled-components/native';
-import Weather from './Weather';
-import Keyword from './Keyword';
-import Shopping from './Shopping';
+import Weather from 'Weather';
+import Keyword from 'Keyword';
+import Shopping from 'Shopping';
+import Geolocation from '@react-native-community/geolocation';
+
+const API_KEY = "b86c474546c60f7c146da98180738950";
 
 const Header = styled.Text`
 color: #000000;
@@ -32,17 +35,55 @@ interface Props {}
 interface State {}
 
 export default class App extends React.Component<Props,State>{
+  state= {
+    isLoaded: false,
+    city: '',
+    weatherName: '',
+    cityTemp: 0,
+    error: null,
+    feels: 0
+  };
+
+  //위치 정보 확인
+  componentDidMount(){
+    Geolocation.getCurrentPosition(
+      position =>{
+        this._getWeather(position.coords.latitude, position.coords.longitude);
+      },
+      error => {
+        this.setState({
+          error: 'error'
+        })
+      }
+    );
+  }
+  _getWeather = (lat, lon) => {
+    fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_KEY}`)
+    .then(response => response.json())
+    .then(json => {
+      this.setState({
+        cityTemp: json.main.temp,
+        weatherName: json.weather[0].main,
+        isLoaded: true,
+        city: json.name,
+        feels: json.main.feels_like
+      })
+    });
+  }
   render(){
+    const { isLoaded, city, weatherName, cityTemp, error, feels } = this.state;
+
     return (
       <SafeAreaView>
         <View style={styles.header}>
         <Header>Today's мода</Header>
-        
         </View>
         <ScrollView>
           <View style={styles.weather}>
-            {/* 날씨 영역 */}
-            <Weather/>
+            {isLoaded ?
+            <Weather city={city} weatherName={weatherName} temp={Math.floor((cityTemp-273.15)*10)/10} feels={Math.floor((feels-273.15)*10)/10}/>
+            : error?<Text>{error}</Text>: null}
+            
           </View>
           <View style={styles.keyword}>
             {/* 키워드 영역 */}
--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,28 +1,76 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+import PropTypes from 'prop-types';
 
-export default class Weather extends React.Component{
-    
-    render(){
-        const {height, width}=Dimensions.get('screen');
-        return (
-            <>
-            <View style={styles.container}>
-                <Image style={styles.weather_img}
-                width={width}
-                height={height/4}
-                source={{
-                    uri: 'https://raw.githubusercontent.com/AYoungSn/Python_project_team6/master/Webcrawling/static/sunny.png',
-                }}
-                />
-                <View style={styles.w_info}>
-                    <Text>체감온도:</Text>
-                </View>
-            </View>
-            </>
-        );
+const weatherCases = {
+    Thunderstorm: {
+        colors: ['#415b72', '#004787'],
+        icon: 'ios-thunderstorm-outline',
+        weatherName: 'Thunderstorm',
+        desc: '오늘은 천둥 번개가 쳐요'    
+    },
+
+    Drizzle: {
+        colors: ['#d7dde5', '#c9d2d8', '#5db7fc'],
+        icon: 'ios-rainy-outline',
+        weatherName: 'Drizzle',
+        desc: '오늘은 이슬비가 내려요'
+    },
+
+    Rain: {
+        colors: ['#00C6FB', '#005BEA'],
+        icon: 'ios-umbrella-outline',
+        weatherName: 'Rain',
+        desc: '오늘은 비가 내려요. 우산을 꼭 챙기세요!'
+    },
+
+    Snow: {
+        colors: ['#7DE2FC', '#B9B6E5'],
+        icon: 'ios-snow-outline',
+        weatherName: 'Snow',
+        desc: '오늘은 눈이 내려요.'
+    },
+
+    Clear: {
+        colors: ['#ffffff', '#6de1ff'],
+        icon: 'ios-sunny-outline',
+        weatherName: 'Clear',
+        desc: '오늘은 맑아요'
+    },
+
+    Clouds: {
+        colors: ['#d7dde5', '#646970'],
+        icon: 'ios-partly-sunny-outline',
+        weatherName: 'Clouds',
+        desc: '오늘은 흐려요'
     }
+
 }
+
+
+function Weather({ city, weatherName, temp, feels}){
+    const {height, width}=Dimensions.get('screen');
+    return (
+        <View style={styles.container}>
+            <Icon name={weatherCases[weatherName].icon} size={height/5} color="black"/>
+            <Text>{weatherName}</Text>
+            <View style={styles.w_info}>
+                <Text>city: {city}</Text>
+                <Text>온도: {temp}°C</Text>
+                <Text>체감온도: {feels}°C</Text>
+            </View>
+        </View>
+    )
+}
+
+Weather.propTypes = {
+    city: PropTypes.string.isRequired,
+    weatherName: PropTypes.string.isRequired,
+    temp: PropTypes.number.isRequired
+}
+
+export default Weather;
 
 const styles = StyleSheet.create({
     container: {
