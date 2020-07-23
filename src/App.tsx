cb55b352a7c28acbea00b01ce303f2ef55a64fbb
Ahn YoungSeon <dudtjs0920@naver.com>
2020-07-23 20:27:04 +0900

    Revert "weather layout 수정"
    This reverts commit 56dc5a79a38ae37d14eebff6cef8f09e03686869.

--- src/App.tsx
+++ src/App.tsx
@@ -24,12 +24,11 @@ const API_KEY = "b86c474546c60f7c146da98180738950";
 
 const Header = styled.Text`
 color: #000000;
-font-size: 25px;
+font-size: 20px;
 text-align: center;
 align-items: center;
 justify-content: center;
 margin: 9px;
-font-weight: bold;
 `;
 
 interface Props {}
@@ -77,13 +76,14 @@ export default class App extends React.Component<Props,State>{
     return (
       <SafeAreaView>
         <View style={styles.header}>
-          <Header>Today's мода</Header>
+        <Header>Today's мода</Header>
         </View>
         <ScrollView>
           <View style={styles.weather}>
             {isLoaded ?
             <Weather city={city} weatherName={weatherName} temp={Math.floor((cityTemp-273.15)*10)/10} feels={Math.floor((feels-273.15)*10)/10}/>
             : error?<Text>{error}</Text>: null}
+            
           </View>
           <View style={styles.keyword}>
             {/* 키워드 영역 */}
@@ -102,7 +102,7 @@ export default class App extends React.Component<Props,State>{
 const styles = StyleSheet.create({
   header: {
     backgroundColor: '#FAFAFA',
-    height: 55
+    height: 45
   },
   weather: {
     
--- src/Weather.tsx
+++ src/Weather.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import {StyleSheet, Text, View, Image, Dimensions} from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
-// https://ionicons.com/   icon 파일명 찾는곳
-
 import PropTypes from 'prop-types';
 
 const weatherCases = {
@@ -51,19 +49,16 @@ const weatherCases = {
 }
 
 
-function Weather({ city, weatherName, temp, feels }){
+function Weather({ city, weatherName, temp, feels}){
     const {height, width}=Dimensions.get('screen');
     return (
         <View style={styles.container}>
-            <View style={styles.weather_img}>
-                <Icon name={weatherCases[weatherName].icon} size={width/3} color="black"/>
-            </View>
-            
+            <Icon name={weatherCases[weatherName].icon} size={height/5} color="black"/>
+            <Text>{weatherName}</Text>
             <View style={styles.w_info}>
-                <Text style={styles.w_txt}>{weatherName}</Text>
-                <Text style={{fontSize: 15}}>city: {city}</Text>
-                <Text style={{fontSize: 20}}>온도: {temp}°C</Text>
-                <Text style={{fontSize: 20}}>체감온도: {feels}°C</Text>
+                <Text>city: {city}</Text>
+                <Text>온도: {temp}°C</Text>
+                <Text>체감온도: {feels}°C</Text>
             </View>
         </View>
     )
@@ -79,25 +74,20 @@ export default Weather;
 
 const styles = StyleSheet.create({
     container: {
-        backgroundColor: 'white',
+        backgroundColor: '#DFFFFF',
         flexDirection: 'row',
         padding: 5,
         margin: 5,
         borderRadius: 5,
     },weather_img: {
+        flex:3,
         margin: 10,
         resizeMode: 'contain',
         alignItems: 'flex-start',
-        color:'black',
     }, w_info: {
         flex:3,
         textAlign: 'left',
         flexDirection: 'column',
-        marginTop: 20,
-        marginLeft: 10,
-        
-    }, w_txt: {
-        fontSize: 20,
-        fontWeight: 'bold',
+        marginTop: 15,
     }
 });
